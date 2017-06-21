168de3dc0ee88cedb94c2d9b4286d5c718bbc543
chepelcr <chepelcr@outlook.com>
2017-06-20 19:00:27 -0600

    Add files via upload

---
+++ Palabras.java
@@ -0,0 +1,55 @@
+//José Pablo Campos Solano
+//Proyecto Programado
+
+import java.util.Scanner;
+    
+public class Palabras
+{
+
+  private String palabra;
+  
+  public Palabras()
+  {
+    this.palabra = "";
+    
+  }//Fin del constructor con parametros
+  
+  
+  public void setPalabra(String palabra)
+  {
+    this.palabra = palabra;
+  }//Fin de setNombre
+  
+  public String getPalabra()
+  {
+    return this.palabra;
+  }//Fin de getNombre
+  
+  public String vocYCons()
+  {
+    
+    Scanner sc =new Scanner(System.in);
+    int contV=0,contC=0;
+    
+    palabra  = getPalabra();
+      
+    palabra = palabra.toLowerCase();
+      
+      for(int i=0;i<palabra.length();i++)
+      {
+        if((palabra.charAt(i)=='a')||(palabra.charAt(i)=='e')||(palabra.charAt(i)=='i')||(palabra.charAt(i)=='o')||(palabra.charAt(i)=='u'))
+        {
+          contV++;
+        }
+        else 
+        {
+          if(palabra.charAt(i)!=' ')
+          {
+          contC++;
+          }
+        }
+      }
+      return "El número de vocales es : " + contV + "\nEl número de consonantes es : " + contC;
+  }//Fin del vocYCons
+  
+}//Fin de la clase
