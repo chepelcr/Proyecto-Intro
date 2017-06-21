3c221b5885e3dce7b3602721f9cf331a3a2725de
chepelcr <chepelcr@outlook.com>
2017-06-21 10:10:14 -0600

    Update Palabras.java

--- Palabras.java
+++ Palabras.java
@@ -5,6 +5,7 @@ public class Palabras
 {
 
   private String palabra;
+  private int vocales, consonantes;
   
   public Palabras(palabra)
   {
@@ -23,6 +24,26 @@ public class Palabras
     return this.palabra;
   }//Fin de getNombre
   
+  public void setVocales(int vocales)
+  {
+    this.vocales = vocales;
+  }//Fin de setVocales
+    
+  public int getVocales()
+  {
+    return this.vocales;
+  }//Fin de getVocales
+    
+  public void setConsonantes(int consonantes)
+  {
+    this.consonantes = consonantes;
+  }//Fin de setConsonantes
+    
+  public int getConsonantes()
+  {
+    return this.consonantes;
+  }//Fin de getConsonantes
+  
   public String vocYCons()
   {
     int contV=0,contC=0;
@@ -36,12 +57,14 @@ public class Palabras
         if((palabra.charAt(i)=='a')||(palabra.charAt(i)=='e')||(palabra.charAt(i)=='i')||(palabra.charAt(i)=='o')||(palabra.charAt(i)=='u'))
         {
           contV++;
+          setVocales();
         }
         else 
         {
           if(palabra.charAt(i)!=' ')
           {
           contC++;
+          setConsonantes();
           }
         }
       }
